d707c2ea9043abf494a3a247a0558b4e77a1e292
hackedbyagirl <brettegeary@gmail.com>
2022-02-06 16:59:36 -0600

    add additional script examples

---
+++ scripts/DisableWinRM.ps1
@@ -0,0 +1,24 @@
+# Taken From https://github.com/ViRb3/windows-ansible/blob/master/DisableWinRM.ps1
+
+# Remove all listeners
+Remove-Item -Path WSMan:\localhost\Listener\* -Recurse -Force
+
+# Undo WinRM settings
+winrm set "winrm/config/service/auth" '@{Basic="false"}'
+winrm set "winrm/config/client/auth" '@{Basic="false"}'
+
+# https://github.com/ansible/ansible/issues/42978
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 0
+
+Remove-NetFirewallRule -DisplayName "HTTPS WinRM"
+
+Set-Service -Name "WinRM" -StartupType Manual
+Stop-Service -Name "WinRM"
+
+# Remove temporary password (if present)
+$username = $env:UserName
+if ((Get-LocalUser -Name $username).Description -eq "WINRM-REMOVE-ME") {
+    Write-Host "Found temporary password for '$username', removing it"
+    Set-LocalUser -name $username -Password ([securestring]::new()) -Description ""
+}
+
